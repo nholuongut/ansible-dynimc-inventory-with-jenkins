3f0259f1b60164b6ab710e937270ada50bec8673
Nho Luong <nholu@nholuongs-MacBook-Pro.local>
2024-10-18 18:19:22 +0700

    Init ansible-dynimc-inventory-with-jenkins

---
+++ terraformscripts/ec2-creation.tf
@@ -0,0 +1,17 @@
+provider "aws"{
+region = "ap-southeast-2"
+}
+
+
+resource "aws_instance" "AWSInstance"{
+     ami = "ami-08cd0547bf85331e7"
+	 instance_type = "t2.micro"
+	 security_groups = ["launch-wizard-2"]
+	 key_name = "devopstrainingmss"
+	 tags = {
+	 Name = "tomcatservers"
+	 }
+
+}
+
+
